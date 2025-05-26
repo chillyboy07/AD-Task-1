baa999390dd1e060151892b482732f67912845a7
chillyboy07 <bonneviechilly@gmail.com>
2025-05-26 18:56:09 +0800

    feat: added composer.json and lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'achilles/ad-task-1',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '97426229e8c36d6179112e2c7b42302d5e773017',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'achilles/ad-task-1' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '97426229e8c36d6179112e2c7b42302d5e773017',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
